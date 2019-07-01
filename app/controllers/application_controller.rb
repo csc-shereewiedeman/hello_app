fdf5f9f4fc181ad3751c751e0bbf489aa91fc946
Sheree Wiedeman <shereewiedeman@digitalglobe.com>
2019-07-01 13:49:27 -0600

    Anything test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hola, mundo!!"
+    render html: "!hola, mundo!!"
   end
 
   def goodbye
-    render html: "adios, mundo!!"
+    render html: "!adios, mundo!!"
   end
 end
 
